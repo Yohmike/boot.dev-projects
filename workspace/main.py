b0a6362d44e2f78187d08888e2a301afc4334b4b
Mike <mihai.plesea@gmail.com>
2024-03-07 11:05:54 +0000

    add report for books

--- workspace/main.py
+++ workspace/main.py
@@ -17,13 +17,26 @@ def get_chars_from_text(text: str) -> Dict[str, int]:
         char_counts[normalized_char] = char_counts.get(normalized_char, 0) + 1
     return char_counts
 
+def build_report(filename: str, word_count: int, chars: Dict[str, int]):
+    print(f"--- Report for book {filename} ---")
+    print(f"\nThere are {word_count} words in the current book.\n")
+
+    for key, value in sorted(chars.items(), key=lambda x: x[1], reverse=True):
+        if key.isalnum():
+            print(f"The {key} char appeared {value} times.")
+    
+    print("--- End report ---")
+
 
 def main():
     book_path = "./books/frankenstein.txt"
+    
     book_contents = open_book(book_path)
-    print(words_in_book(book_contents))
+    
+    words = words_in_book(book_contents)
     chars = get_chars_from_text(book_contents)
-    print(chars)
+    
+    build_report(book_path, words, chars)
 
 
 if __name__ == "__main__":
