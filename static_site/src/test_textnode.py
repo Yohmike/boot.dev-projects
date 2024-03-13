5926f640b542c37bf798ace4c162ac0b208a69e1
Mike <mihai.plesea@gmail.com>
2024-03-13 12:49:48 +0000

    add split delimiter ch3:1

--- static_site/src/test_textnode.py
+++ static_site/src/test_textnode.py
@@ -1,29 +1,107 @@
 import unittest
 
-from textnode import TextNode
+from textnode import TextNode, split_nodes_delimiter, \
+    text_type_bold, text_type_text, text_type_italic, text_type_code
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold")
+        node = TextNode("This is a text node", text_type_bold)
+        node2 = TextNode("This is a text node", text_type_bold)
         self.assertEqual(node, node2)
     
     def test_neq_text(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is another text node", "bold")
+        node = TextNode("This is a text node", text_type_bold)
+        node2 = TextNode("This is another text node", text_type_bold)
         self.assertNotEqual(node, node2)
     
     def test_neq_type(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "italic")
+        node = TextNode("This is a text node", text_type_bold)
+        node2 = TextNode("This is a text node", text_type_italic)
         self.assertNotEqual(node, node2)
     
     def test_neq_url(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold", url="www.google.com")
+        node = TextNode("This is a text node", text_type_bold)
+        node2 = TextNode(
+            "This is a text node", text_type_bold, url="www.google.com"
+            )
         self.assertNotEqual(node, node2)
 
+class TestSplitNodes(unittest.TestCase):
+    def test_simple_node(self):
+        node = TextNode("This is a text node", text_type_text)
+        nodes = split_nodes_delimiter([node], "", text_type_text)
+        self.assertEqual(nodes, [node])
+    
+    def test_simple_bold(self):
+        node = TextNode("This is a **bold** textnode", text_type_text)
+        nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        expected_nodes = [
+            TextNode("This is a ", text_type_text),
+            TextNode("bold", text_type_bold),
+            TextNode(" textnode", text_type_text)
+        ]
+        self.assertEqual(nodes, expected_nodes)
 
+    def test_starts_with_bold(self):
+        node = TextNode("**bold** textnode", text_type_text)
+        nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        expected_nodes = [
+            TextNode("bold", text_type_bold),
+            TextNode(" textnode", text_type_text)
+        ]
+        self.assertEqual(nodes, expected_nodes)
+
+    def test_ends_with_bold(self):
+        node = TextNode("this is very **bold**", text_type_text)
+        nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        expected_nodes = [
+            TextNode("this is very ", text_type_text),
+            TextNode("bold", text_type_bold)
+        ]
+        self.assertEqual(nodes, expected_nodes)
+    
+    def test_multiple_types(self):
+        node = TextNode("this is very **bold** and **bold**", text_type_text)
+        nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        expected_nodes = [
+            TextNode("this is very ", text_type_text),
+            TextNode("bold", text_type_bold),
+            TextNode(" and ", text_type_text),
+            TextNode("bold", text_type_bold)
+        ]
+        self.assertEqual(nodes, expected_nodes)
+    
+    def test_multiple_different_types(self):
+        node = TextNode(
+            "this is very **bold** and *italic* and `code`", text_type_text
+            )
+        nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        self.assertEqual(nodes, [
+            TextNode("this is very ", text_type_text),
+            TextNode("bold", text_type_bold), 
+            TextNode(" and *italic* and `code`", text_type_text)
+            ])
+        nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+        self.assertEqual(nodes, [
+            TextNode("this is very ", text_type_text),
+            TextNode("bold", text_type_bold), 
+            TextNode(" and ", text_type_text),
+            TextNode("italic", text_type_italic),
+            TextNode(" and `code`", text_type_text)
+            ])
+        nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+        expected_nodes = [
+            TextNode("this is very ", text_type_text),
+            TextNode("bold", text_type_bold),
+            TextNode(" and ", text_type_text),
+            TextNode("italic", text_type_italic),
+            TextNode(" and ", text_type_text),
+            TextNode("code", text_type_code),
+        ]
+        #self.assertEqual(nodes, expected_nodes)
+    
+    
+    
 if __name__ == "__main__":
     unittest.main()
--- static_site/src/textnode.py
+++ static_site/src/textnode.py
@@ -1,5 +1,21 @@
-from typing import Optional
+from typing import Optional, List
 from htmlnode import LeafNode
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+valid_text_types = [
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_link,
+    text_type_image
+    ]
 
 
 class TextNode:
@@ -17,16 +33,53 @@ class TextNode:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
     
 
-def text_node_to_html_node(text_node):
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
     valid_types = {
-        "text": LeafNode(tag=None, value=text_node.text),
-        "bold": LeafNode(tag="b", value=text_node.text),
-        "italic": LeafNode(tag="i", value=text_node.text),
-        "code": LeafNode(tag="code", value=text_node.text),
-        "link": LeafNode(tag="a", value=text_node.text, props={"href":text_node.url}),
-        "image": LeafNode(tag="img", value=None, props={"src": text_node.url, "alt": text_node.text})
+        text_type_text: LeafNode(tag=None, value=text_node.text),
+        text_type_bold: LeafNode(tag="b", value=text_node.text),
+        text_type_italic: LeafNode(tag="i", value=text_node.text),
+        text_type_code: LeafNode(tag="code", value=text_node.text),
+        text_type_link: LeafNode(
+            tag="a", value=text_node.text, props={"href": text_node.url}),
+        text_type_image: LeafNode(
+            tag="img", 
+            value=None, 
+            props={"src": text_node.url, "alt": text_node.text})
     }
     if valid_types.get(text_node.text_type) is None:
         raise Exception(f"Invalid type :{text_node.text_type}")
     else:
         return valid_types[text_node.text_type]
+
+
+def split_nodes_delimiter(
+        old_nodes: List[TextNode], 
+        delimiter: str, 
+        text_type: str
+        ) -> List[TextNode]:
+    new_nodes = []
+    if not delimiter:
+        return old_nodes
+    for node in old_nodes:
+        if not isinstance(node, TextNode) or \
+            (isinstance(node, TextNode) and node.text_type != text_type_text):
+            new_nodes.append(node)
+        else:
+            split_nodes = []
+            text_to_split = node.text
+            if text_to_split.count(delimiter) % 2 != 0:
+                raise Exception(
+                    f"closing delimiter \"{delimiter}\" not found \
+                      for \"{text_to_split}\"")
+            else:
+                parts = text_to_split.split(delimiter)
+                for i in range(0, len(parts)):
+                    if i % 2 == 0:
+                        if parts[i]:
+                            split_nodes.append(
+                                TextNode(parts[i], text_type_text)
+                            )
+                    else:
+                        split_nodes.append(TextNode(parts[i], text_type))
+            new_nodes.extend(split_nodes)
+    return new_nodes
