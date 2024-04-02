64273219e0aeb506c37d8eae6480f02b211f7ff1
Mike <mihai.plesea@gmail.com>
2024-04-02 15:12:42 +0100

    Add cell and cell center draw function ch5,6

--- maze/basic_graphics.py
+++ maze/basic_graphics.py
@@ -58,3 +58,72 @@ class Line:
 
     def __repr__(self) -> str:
         return f"Line( from {self.__from} to {self.__to})"
+
+
+class Cell:
+    def __init__(self, bottom_right: Point, top_left: Point, window: Window) -> None:
+        self.has_left_wall = False
+        self.has_right_wall = False
+        self.has_top_wall = False
+        self.has_bottom_wall = False
+
+        self._x1, self._y1 = bottom_right.y, bottom_right.x
+        self._x2, self._y2 = top_left.y, top_left.x
+        self._win = window
+
+    def draw(self, fill_color: str = "black") -> None:
+        top_right = Point(self._x1, self._y2)
+        top_left = Point(self._x1, self._y1)
+        bottom_right = Point(self._x2, self._y2)
+        bottom_left = Point(self._x2, self._y1)
+        if self.has_left_wall:
+            left_line = Line(from_point=top_left, to_point=bottom_left)
+            self._win.draw_line(left_line, fill_color=fill_color)
+        if self.has_right_wall:
+            right_line = Line(from_point=top_right, to_point=bottom_right)
+            self._win.draw_line(right_line, fill_color=fill_color)
+        if self.has_top_wall:
+            top_line = Line(from_point=top_left, to_point=top_right)
+            self._win.draw_line(top_line, fill_color=fill_color)
+        if self.has_bottom_wall:
+            bottom_line = Line(from_point=bottom_left, to_point=bottom_right)
+            self._win.draw_line(bottom_line, fill_color=fill_color)
+    
+    def add_walls(self, walls: str) -> None:
+        if "N" in walls:
+            self.has_top_wall = True
+        if "E" in walls:
+            self.has_right_wall = True
+        if "S" in walls:
+            self.has_bottom_wall = True
+        if "W" in walls:
+            self.has_left_wall = True
+    
+    def get_center(self):
+        if self._x1 == self._x2 == 0:
+            x = 0
+        elif self._x1 > self._x2:
+            x = self._x1 - (self._x1 - self._x2) / 2
+        elif self._x1 <= self._x2:
+            x = self._x2 - (self._x2 - self._x1) / 2
+        
+        if self._y1 == self._y2 == 0:
+            y = 0
+        elif self._y1 > self._y2:
+            y = self._y1 - (self._y1 - self._y2) / 2
+        elif self._y1 <= self._y2:
+            y = self._y2 - (self._y2 - self._y1) / 2
+        
+        return Point(x, y)
+
+    
+    def draw_move(self, to_cell, undo=False):
+        color = None
+        if undo:
+            color = "gray"
+        else:
+            color = "red"
+        center1 = self.get_center()
+        center2 = to_cell.get_center()
+
+        self._win.draw_line(Line(center1, center2), color)
--- maze/main.py
+++ maze/main.py
@@ -1,4 +1,7 @@
-from basic_graphics import Window, Line, Point
+from basic_graphics import Window, Line, Point, Cell
+
+
+
 def main():
     width = 800
     height = 600
@@ -9,8 +12,41 @@ def main():
     top_left_corner = Point(0, height)
     top_right_corner = Point(width, height)
 
-    win.draw_line(Line(bottom_left_corner, top_right_corner),fill_color="green")
-    win.draw_line(Line(top_left_corner, bottom_right_corner),fill_color="red")
+    # win.draw_line(Line(bottom_left_corner, top_right_corner),fill_color="green")
+    # win.draw_line(Line(top_left_corner, bottom_right_corner),fill_color="red")
+
+    w_cell = height / 10 + 1
+    h_cell = width / 10 + 1
+
+    cells = [
+        (Point(1, 1),Point(w_cell, h_cell), "red", ""),
+        (Point(1, 2 * h_cell),Point( w_cell, 3 * h_cell), "green", "N"),
+        (Point(1, 4 * h_cell),Point( w_cell, 5 * h_cell), "green", "E"),
+        (Point(1, 6 * h_cell),Point( w_cell, 7 * h_cell), "green", "W"),
+        (Point(1, 8 * h_cell),Point( w_cell, 9 * h_cell), "green", "S"),
+        (Point(2 * w_cell, 1),Point( 3 * w_cell, h_cell), "red", "NE"),
+        (Point(2 * w_cell, 2 * h_cell),Point( 3 * w_cell, 3 * h_cell), "red", "ES"),
+        (Point(2 * w_cell, 4 * h_cell),Point( 3 * w_cell, 5 * h_cell), "red", "SW"),
+        (Point(2 * w_cell, 6 * h_cell),Point( 3 * w_cell, 7 * h_cell), "red", "WN"),
+        (Point(2 * w_cell, 8 * h_cell),Point( 3 * w_cell, 9 * h_cell), "red", "NS"),
+        (Point(4 * w_cell, 0 * h_cell + 1),Point( 5 * w_cell, 1 * h_cell), "red", "EW"),
+        (Point(4 * w_cell, 2 * h_cell),Point( 5 * w_cell, 3 * h_cell), "blue", "NES"),
+        (Point(4 * w_cell, 4 * h_cell),Point( 5 * w_cell, 5 * h_cell), "blue", "ESW"),
+        (Point(4 * w_cell, 6 * h_cell),Point( 5 * w_cell, 7 * h_cell), "blue", "SWN"),
+        (Point(4 * w_cell, 8 * h_cell),Point( 5 * w_cell, 9 * h_cell), "blue", "WNE"),
+        (Point(6 * w_cell, 0 * h_cell + 1),Point( 7 * w_cell, 1 * h_cell), "black", "SWNE"),
+    ]
+    draw_cells = []
+    for cell in cells:
+        br, tl, fill_color, walls = cell
+        draw_cell = Cell(bottom_right=br, top_left=tl, window=win)
+        draw_cell.add_walls(walls=walls)
+        draw_cell.draw(fill_color=fill_color)
+        draw_cells.append(draw_cell)
+    
+    draw_cells[5].draw_move(draw_cells[10])
+
+
     win.wait_for_close()
 
 if __name__ == "__main__":
